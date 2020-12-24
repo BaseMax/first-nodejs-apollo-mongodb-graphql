c6489986c4f58535d041f1e1374aca6dd8cd332b
Max Base <MaxBaseCode@Gmail.Com>
2020-12-24 05:27:46 +0330

    Adding createCat mutation

--- resolvers.js
+++ resolvers.js
@@ -1,8 +1,20 @@
+import { Cat } from './models/Cat'
+
 export const resolvers = {
   Query: {
-    hello: () => `Hello, World`
+    hello: () => `Hello, World`,
+    cats: () => {
+      console.log('select from Cats')
+      return Cat.find()
+    }
+  },
+  Mutation: {
+    createCat: async (_, {name}) => {
+    const cat = new Cat({name})
+    await cat.save()
+    console.log(cat)
+    return cat
+    }
   }
 }
 
-// const kitty = new Cat({ name: 'Zildjian' })
-// kitty.save().then(() => console.log('meow'))
